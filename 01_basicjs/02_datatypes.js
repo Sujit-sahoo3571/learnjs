cc9481a0dbdbd6e3446916a07532fe8a8ed732a0
Sujit <55569677+Sujit-sahoo3571@users.noreply.github.com>
2023-10-29 10:31:47 +0000

    basic data type that work

---
+++ 01_basicjs/02_datatypes.js
@@ -0,0 +1,13 @@
+// "use strict"; // use to say the compile for new standard js
+// alert('welcome') // error not define // we are working in node not in browser
+console.log("Welcome to js ")
+let name = "sujit"
+let age = 18
+let balance = 5690.90
+let isLogin = false;
+let isError = true; 
+let definy = undefined;
+let str = null; 
+// symbol  to mark as unique 
+
+console.table([name, age, balance, isLogin , isError , definy, str ]);
