6ad6e020c01016421e7a882e46449cb994438ea6
Sujit <55569677+Sujit-sahoo3571@users.noreply.github.com>
2023-10-29 06:25:33 +0000

    variables

---
+++ 01_basicjs/01_variables.js
@@ -0,0 +1,15 @@
+const studentId = 13433;
+let studentName = 'sujit kumar sahoo';
+var emailId = 'sahoo@7899.com';
+password = "Believe In yourself "
+
+
+console.table([studentName, studentId, emailId, password ])
+
+// studentId = 8088; // can't assign to a const variable 
+studentName ='Believer_7855';
+emailId='badass@6766.com';
+password='withinyou';
+
+console.log(studentId);
+console.table([studentName, studentId, emailId, password ])
