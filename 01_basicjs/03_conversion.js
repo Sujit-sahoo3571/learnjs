57e551f767700bb220e9ac969fc249dc96a3e9b6
Sujit <55569677+Sujit-sahoo3571@users.noreply.github.com>
2023-10-29 10:50:21 +0000

    type conversion

---
+++ 01_basicjs/03_conversion.js
@@ -0,0 +1,10 @@
+// let num  = "34";
+// console.log(typeof num);
+// console.log(typeof Number(num ));
+
+// let str = "34rrkjdf"
+// console.log(typeof Number(str)) // Number => NaN 
+// console.log(Number(str))
+
+// let isOk  = true
+// console.log(Boolean(isOk))
