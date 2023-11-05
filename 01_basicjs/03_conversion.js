69c48c1e3376cd3dcf5fb57f0aa9c8e117d6b93c
Sujit <55569677+Sujit-sahoo3571@users.noreply.github.com>
2023-11-05 12:20:38 +0000

    fibbonacci series

--- 01_basicjs/03_conversion.js
+++ 01_basicjs/03_conversion.js
@@ -8,3 +8,36 @@
 
 // let isOk  = true
 // console.log(Boolean(isOk))
+
+
+/**
+ * fibbonacci series
+ * 0 1 1 2 3 5 8 13 21  
+ */
+
+
+let a = 0
+let b  = 1 
+let c = 0 
+
+// console.log(a +" "+ b );
+
+
+let alist = [a, b ];
+
+for (let i = 0 ; i < 10 ; i++ ){
+    c = a+ b ;
+    // console.log(c );
+    alist.push(c)
+a = b 
+b = c 
+
+}
+
+// console.log(alist);
+console.log(alist.toString());
+
+
+
+
+
---
+++ 01_basicjs/myfibbonaci.js
@@ -0,0 +1,13 @@
+let a = 0; 
+let b =1; 
+let c = 0; 
+
+for(let i = 0; i< 10; i++ ){
+  //  c = a+b 
+    //a =b 
+    //b = c 
+    console.log(c ); 
+    a  = b 
+    b = c 
+    c = a+b 
+}
